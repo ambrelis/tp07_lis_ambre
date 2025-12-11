ce819738c2c6274039b3d3dc5502fb1f8cc9ae31
Ambre Lis <alis@cfai-formation.fr>
2025-12-11 14:13:24 +0100

    ajout du tp4

---
+++ front/src/main.ts
@@ -0,0 +1,13 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient()]
+}).catch(err => console.error(err));
